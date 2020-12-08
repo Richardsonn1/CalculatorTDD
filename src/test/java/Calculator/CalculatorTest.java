5cb2a1ca78a60041ec12c3d9aff3f6b6b79d6ae5
Marcus <marcus.richardson@hotmail.se>
2020-12-08 19:07:34 +0100

    UnkownInput test fail
    UnkownInput test fail because the method can only handle 2 integers atm.

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package Calculator;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class CalculatorTest {
 
     Calculator calculator = new Calculator();
@@ -19,5 +21,20 @@ public class CalculatorTest {
     @Test
     void TwoIntegersInputWithDelimiterShouldReturnSum(){
         assertEquals(3, calculator.add("1,2"), "Sum by 1 + 2 should be 3.");
+    }
+
+    @Test
+    void UnknownAmountOfIntegerShouldReturnCorrectValue(){
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        int rng = random.nextInt(10);
+        int expected = 0;
+        for(int i = 0; i < rng; i++){
+            sb.append(i).append(",");
+            expected = expected + i;
+        }
+        String numbers = String.valueOf(sb);
+        int result = calculator.add(numbers);
+        assertEquals(expected, result, "Sum of String numbers & expected should be the same.");
     }
 }
