ebfb1156a0a8a39daeafa7baf771e7ea031194f2
Marcus <marcus.richardson@hotmail.se>
2020-12-08 20:24:57 +0100

    Updated getSum
    Updated getSum method to handle and throw exception if negative integers are used.

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,5 +1,7 @@
 package Calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
     public int add(String input) {
         if (input.isEmpty()) {
@@ -11,6 +13,16 @@ public class Calculator {
 
     private int getSum(String input) {
         String[] splitNumbers = input.split("[,\n/;]");
+        ArrayList<Integer> negativeNumbers = new ArrayList<>();
+        for (int i = 0; i < splitNumbers.length; i++) {
+            String number = splitNumbers[i];
+            if (!number.isEmpty() && Integer.parseInt(number) < 0) {
+                negativeNumbers.add(Integer.parseInt(number));
+            }
+        }
+        if(0 < negativeNumbers.size()){
+            throw new IllegalArgumentException("Negatives not allowed! Negatives used: " + negativeNumbers.toString());
+        }
         int sum = 0;
         for (String splitNumber : splitNumbers) {
             if (!splitNumber.isEmpty()) {
