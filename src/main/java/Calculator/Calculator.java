d9a0b01d08f2f1b8894f76d33a89ff7027c09ff3
Marcus <marcus.richardson@hotmail.se>
2020-12-08 15:09:04 +0100

    TwoIntegers test OK & refactored.
    Test is OK.
    Updated code to seperate input at commas.

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -4,8 +4,12 @@ public class Calculator {
     public int add(String input) {
         if(input.isEmpty()) {
             return 0;
-        }else{
+        }
+        if(input.length() == 1){
             return Integer.parseInt(input);
+        }else{
+            String[] splitNumbers = input.split(",");
+            return Integer.parseInt(splitNumbers[0]) + Integer.parseInt(splitNumbers[1]);
         }
     }
 }
