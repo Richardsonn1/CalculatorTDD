5c2df2e8a821da4ddd35699825d3d3ca46ea6577
Marcus <marcus.richardson@hotmail.se>
2020-12-08 14:56:33 +0100

    OneInteger test passed & refactored.
    Refactored code in test class.

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void EmptyLineInputReturnsZero(){
-        Calculator calculator = new Calculator();
         assertEquals(0,calculator.add(""), "Sum by nothing should be 0.");
     }
 
     @Test
     void OneIntegerInputShouldReturnSameValue(){
-        Calculator calculator = new Calculator();
-        String input = "1";
-        int result = calculator.add(input);
-        assertEquals(1,result,"1 + nothing should return 1.");
+        assertEquals(1,calculator.add("1"),"1 + nothing should return 1.");
     }
 }
